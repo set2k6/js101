d0dac2deabea856ae0a8a139458da94b78275b24
set2k6 <set2k6@gmail.com>
2016-04-11 23:43:28 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,102 @@
+
+// How many hours are in this year
+
+//var hoursinayear = 8760;
+
+/*for later
+var yearMinutes = oneYear / oneHour
+console.log ("one year in hours", yearMinutes); */
+
+// How many minutes are in a decade without leap years
+
+/*var DecadeMinutes = oneYear * 10 / oneMinute
+console.log ("minutes/decades", decadeMinutes);*/
+
+
+// How many seconds old I am
+
+//var ageSeconds = oneYear * myAge / oneSecond
+
+
+// If I am older than some arbitrary number, alert "I'm old", else "I'm young"
+
+/*if (myAgeinMinutes >= 600000000000) {
+	console.log ("", "I'm old");
+
+} else  {
+	console.log ("", "I'm young");
+}
+*/
+var num= 20;
+if (27 > num) {
+	console.log ("I am old");
+} else {
+	console.log ("I am young")
+}
+
+//in class note taking
+
+var myArray = [2, "1", 4, true, "hello"]
+
+console.log ("array", myArray);
+
+var phrase = "the quick brown fox jumps over the lazy dog!";
+
+console.log("phrase length", phrase.length);
+
+
+var phrase = document.getElementById("myPhrase");
+console.log("phrase",phrase);
+
+var innerPhrase = phrase.innerHTML;
+
+console.log("innerPhrase", innerPhrase);
+
+phrase.innerHTML = "Javascript just changed my phrase";
+
+console.log("innerPhrase", innerPhrase);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
